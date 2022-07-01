1b1d134252ada7b1a61c6aab899fde35de0025fc
caediteeos <dzhanga9@gmail.com>
2022-07-01 16:06:03 +0600

    upload lab4

---
+++ lab4/onlineStore/src/app/products.ts
@@ -0,0 +1,99 @@
+export const products = [
+  {
+    id: 1,
+    name: 'Acer SB220Q bi 21.5 Inches Full HD (1920 x 1080) IPS Ultra-Thin Zero Frame Monitor (HDMI & VGA Port), Black',
+    price: 93.80,
+    description: 'The Acer SB220Q 21.5" Widescreen IPS display combines stylish ultra-thin functionality with amazing 1920 x 1080 resolution, allowing you to enjoy High-Definition entertainment and gaming in the comfort of your home. AMD Radeon FreeSync and rapid 4ms response time highlight the advanced technology.',
+    rating: 4.7,
+    link: 'https://www.amazon.com/Acer-SB220Q-Ultra-Thin-Frame-Monitor/dp/B07CVL2D2S/ref=sr_1_2?crid=TCDCS6ONQD5B&dchild=1&keywords=monitor&qid=1613997406&sprefix=mon%2Caps%2C328&sr=8-2',
+    image: 'https://images-na.ssl-images-amazon.com/images/I/81QpkIctqPL._AC_SL1500_.jpg'
+  },
+  {
+    id: 2,
+    name: 'OneOdio Wired Over Ear Headphones Studio Monitor & Mixing DJ Stereo Headsets with 50mm Neodymium Drivers and 1/4 to 3.5mm Audio Jack for AMP Computer Recording Phone Piano Guitar Laptop - Black',
+    price: 30.99,
+    description: 'BASS SOUND: Enjoy clear sound and comfort with the OneOdio Studio monitor headphones. Large, 50 millimeter speaker unit drivers combined with neodymium magnets; powerful bass, clear vocal, and crisp high tones form stereo sound. It is ideal for DJ digital mixer, mixer panel, turntable, monitoring in recording studios, radio, film production, video, electronic news gathering--and virtually any application where high quality sound is required.',
+    rating: 4.5,
+    link: 'https://www.amazon.com/OneOdio-Adapter-Free-Headphones-Professional-Telescopic/dp/B01N6ZJH96/ref=sr_1_1_sspa?dchild=1&keywords=headphones&qid=1614005830&sr=8-1-spons&psc=1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUEzM1AyRzRCRjk0Q1VZJmVuY3J5cHRlZElkPUEwOTgwNDM5M01GVFpHTU9RMVYxMSZlbmNyeXB0ZWRBZElkPUEwNTE3ODYwMk8wQkU3M0QxRkxFSCZ3aWRnZXROYW1lPXNwX2F0ZiZhY3Rpb249Y2xpY2tSZWRpcmVjdCZkb05vdExvZ0NsaWNrPXRydWU=',
+    image: 'https://images-na.ssl-images-amazon.com/images/I/81xdLjyV5%2BL._AC_SY355_.jpg'
+  },
+  {
+    id: 3,
+    name: 'Smart Watch, Popglory Smartwatch with Blood Pressure, Blood Oxygen Monitor, Fitness Tracker with Heart Rate Monitor, Full Touch Fitness Watch for Android & iOS for Men Women',
+    price: 32.99,
+    description: 'Monitor Blood Pressure and Blood Oxygen: Adopting advanced sensors, the watch can detect your blood pressure and blood oxygen while record the data in its companion app HeroBandⅢ( Please scan the QR code in the manual to download the app), which help you know body changes better. NOTE the data cannot be used as a medical-grade test.',
+    rating: 4,
+    link: 'https://www.amazon.com/Popglory-Smartwatch-Pressure-Monitor-Fitness/dp/B08DXS7VCK/ref=sr_1_1_sspa?crid=1NDS4GJ8UAALT&dchild=1&keywords=apple+watch&qid=1614006308&sprefix=apple%2Caps%2C328&sr=8-1-spons&psc=1&smid=A3QEK68Z4UXCRO&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUFORk5RV0c1SEREMUEmZW5jcnlwdGVkSWQ9QTA4NDAxNzMxU09WSzg4Qk1CUlRSJmVuY3J5cHRlZEFkSWQ9QTA4NjAyMDMyVVlVM01PV0YySURLJndpZGdldE5hbWU9c3BfYXRmJmFjdGlvbj1jbGlja1JlZGlyZWN0JmRvTm90TG9nQ2xpY2s9dHJ1ZQ==',
+    image: 'https://images-na.ssl-images-amazon.com/images/I/71XdYJvmqBL._AC_SX679_.jpg'
+  },
+  {
+    id: 4,
+    name: 'Dell Inspiron 17 17.3" i3793-7275SLV-PUS 10th Gen Intel Core i7-1065G7 16GB RAM 2TB HDD + 256GB SSD DVD-RW 2GB NVIDIA MX230 (1920 x 1080) Display Windows 10 Home',
+    price: 984.49,
+    description: 'Processor & Memory: 10th Gen Intel Core i7-1065G7 Processor 1.3GHz 16GB 2666MHz DDR4 RAM Drives: 2.0TB SATA 5400 RPM Hard Drive + 256GB PCIe NVMe M.2 Solid State Drive DVD-RW (Writes to DVD/CD) Operating System: Microsoft Windows 10 Home (64-bit) Communications: 802.11 Wireless-AC Qualcomm DW1810 LAN + Bluetooth 4.1 Integrated Widescreen HD 720p Webcam with Dual Digital Microphone Array 10/100 Ethernet LAN Audio Stereo Speakers with MaxxAudio Pro Graphics & Video: 17.3" Anti-Glare LED-Backlit FHD (1920 x 1080) Display 2GB NVIDIA GeForce MX230 Graphics Keyboard: Standard Keyboard Ports & Slots: 2x USB 3.1 Gen 1 1x USB 3.1 Gen 1 Type-C with Data Transition Only 1x USB 2.0 1x RJ-45 1x HDMI Out 1.4b 1x Combination Headphone/Microphone Jack 1x Media Card Reader Power Supply: 3-Cell 42WHr Battery Additional Information: Dimensions: 16.35" x 10.99" x 0.98" Approximate Weight: 6.13 lbs',
+    rating: 4.3,
+    link: 'https://www.amazon.com/Dell-Inspiron-i3793-7275SLV-PUS-i7-1065G7-Display/dp/B081K9QZDF/ref=sr_1_8?dchild=1&keywords=laptop&qid=1614006639&sr=8-8',
+    image: 'https://images-na.ssl-images-amazon.com/images/I/51cUNtjCjVL._AC_SX522_.jpg'
+  },
+  {
+    id: 5,
+    name: 'Fire HD 8 Plus tablet, HD display, 64 GB, our best 8" tablet for portable entertainment, Slate + Made for Amazon, Wireless Charging Dock',
+    price: 169.99,
+    description: 'With 3 GB RAM and a powerful quad-core processor, Fire HD 8 Plus delivers a smooth experience whether you’re browsing the web or binge-watching your favorite show. Download and stream more with the all-day battery and up to 1 TB of expandable storage. Just place your tablet in the made-for-Amazon charging dock (sold separately) or another Qi-certified wireless charger to power up. Fire HD 8 Plus switches to Show Mode in the dock so you can use it hands-free with Alexa.',
+    rating: 4.5,
+    link: 'https://www.amazon.com/Fire-HD-8-Plus-tablet-and-Wireless-Charging-Dock/dp/B087F1SVDZ/ref=sr_1_17?crid=33GBMOXMEYDIW&dchild=1&keywords=tablet&qid=1614006759&s=pc&sprefix=table%2Caps%2C371&sr=1-17',
+    image: 'https://images-na.ssl-images-amazon.com/images/I/41xpGPy6dCL._AC_.jpg'
+  },
+  {
+    id: 6,
+    name: 'Veryhome Fake Succulent Plants Artificial Faux Succulents 4pcs Plastic Mini Potted Fake Succulents for Flower Arrangements',
+    price: 11.29,
+    description: 'REALISTIC and REAL TOUCH】These artificial succulents has life-like looking , vivid color,real touch material.Mini fake succulents are the best choice for those who shares no time for live succulents.These faux succulents potted are really very cute,also Safer than real ones for houses with pets. Stay close to nature without watering the floral plants.',
+    rating: 4.3,
+    link: 'https://www.amazon.com/Veryhome-Fake-Succulent-Plants-Artificial-Faux-Succulents-4pcs-Plastic-Mini-Potted-Fake-Succulents-Flower-Arrangements/dp/B07DXP3KMG/ref=sr_1_2?dchild=1&keywords=plants&qid=1614006896&sr=8-2',
+    image: 'https://images-na.ssl-images-amazon.com/images/I/61tgGHypqML._AC_SL1500_.jpg'
+  },
+  {
+    id: 7,
+    name: 'Mr. Coffee One-Touch CoffeeHouse Espresso Maker and Cappuccino Machine',
+    price: 223.98,
+    description: 'Skip the trip to or long lines at the local coffee shop and easily create extraordinary coffee drinks at home with Mr. Coffee One-Touch CoffeeHouse. With a simple press of a button, you can make espressos, cappuccinos, and lattes within minutes. Choose from a single or double shot of espresso and then let the automatic milk frother steam the milk of your choice into a light and airy foam. A 19-bar Italian pump delivers the optimal amount of pressure to squeeze every bit of flavor from the brew for delicious barista-quality drinks.',
+    rating: 4.4,
+    link: 'https://www.amazon.com/Mr-Coffee-One-Touch-CoffeeHouse-Cappuccino/dp/B07CJ3CYF7/ref=sr_1_1_sspa?dchild=1&keywords=coffee+machine&qid=1614007104&refinements=p_36%3A1253527011&rnid=386465011&s=home-garden&sr=1-1-spons&psc=1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUEzVk9LOFBCR084OE85JmVuY3J5cHRlZElkPUEwNjM3MzMzMlVWSDVVTlhWTEpGNSZlbmNyeXB0ZWRBZElkPUEwNDU1MDM2SjBTN0FZWEFaMTNQJndpZGdldE5hbWU9c3BfYXRmJmFjdGlvbj1jbGlja1JlZGlyZWN0JmRvTm90TG9nQ2xpY2s9dHJ1ZQ==',
+    image: 'https://m.media-amazon.com/images/S/aplus-media/vc/4d063640-9df9-4e35-9aaf-0586f434982f._CR0,0,570,570_PT0_SX300__.jpg'
+  },
+  {
+    id: 8,
+    name: 'SAMSUNG QN32Q50RAFXZA Flat 32" QLED 4K 32Q50 Series Smart TV',
+    price: 447.99,
+    description: 'Buy, rent and watch the newest movies, plus subscribe to and watch all new Apple TV channels like HBO, Showtime and Starz on the new Apple TV app. Plus with AirPlay 2-enabled Samsung Smart TVs, customers can also effortlessly play videos from their Apple device directly to their Samsung Smart TV.',
+    rating: 4.7,
+    link: 'https://www.amazon.com/SAMSUNG-QN32Q50RAFXZA-32Q50-Smart-TV2019/dp/B07W5QYD2K/ref=sr_1_1?dchild=1&keywords=tv+65+inch+smart+tv+4k&qid=1614007487&s=electronics&sr=1-1',
+    image: 'https://m.media-amazon.com/images/S/aplus-media/vc/1b2b5fe9-1f15-4e52-af21-89d4ab192c8b.__CR0,0,1000,750_PT0_SX800_V1___.jpg'
+  },
+  {
+    id: 9,
+    name: 'Assassin’s Creed Valhalla PlayStation 5 Standard Edition',
+    price: 39.88,
+    description: 'Lead epic Viking raids against Saxon troops and fortresses. Relive the visceral fighting style of the Vikings as you dual-wield powerful weapons. Challenge yourself with the most varied collection of enemies ever in Assassins Creed. Shape the growth of your character and your clans settlement with every choice you make. Explore a Dark Age open world, from the harsh shores of Norway to the beautiful kingdoms of England',
+    rating: 4.7,
+    link: 'https://www.amazon.com/Assassins-Creed-Valhalla-PlayStation-5-Standard/dp/B08FS5HKTR/ref=sr_1_6?crid=12C7P1TRK205B&dchild=1&keywords=playstation+5&qid=1614007572&sprefix=play%2Caps%2C355&sr=8-6',
+    image: 'https://images-na.ssl-images-amazon.com/images/I/81q2v4mcZSL._SL1500_.jpg'
+  },
+  {
+    id: 10,
+    name: '2020 Apple MacBook Air with Apple M1 Chip (13-inch, 8GB RAM, 256GB SSD Storage) - Gold',
+    price: 987.50,
+    description: 'Apple-designed M1 chip for a giant leap in CPU, GPU, and machine learning performance. Go longer than ever with up to 18 hours of battery life. 8-core CPU delivers up to 3.5x faster performance to tackle projects faster than ever. Up to eight GPU cores with up to 5x faster graphics for graphics-intensive apps and games. 16-core Neural Engine for advanced machine learning. 8GB of unified memory so everything you do is fast and fluid. Superfast SSD storage launches apps and opens files in an instant.',
+    rating: 4.8,
+    link: 'https://www.amazon.com/Apple-MacBook-13-inch-256GB-Storage/dp/B08N5M7S6K/ref=sr_1_2?dchild=1&keywords=laptop&qid=1614008723&sr=8-2&th=1',
+    image: 'https://images-na.ssl-images-amazon.com/images/I/71vFKBpKakL._AC_SL1500_.jpg'
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
